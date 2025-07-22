39e989a9d92f06dbde4d4e1b09370e432c5b83fe
Muqadas Urooj <104297720+muqadasurooj512@users.noreply.github.com>
2025-07-21 21:57:46 -0700

    updated

---
+++ src/Day10.jsx
@@ -0,0 +1,15 @@
+// Create a modal popup that opens when a button is pressed.
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Day10 = () => {
+  return (
+    <View>
+      <Text>Day10</Text>s
+    </View>
+  )
+}
+
+export default Day10
+
+const styles = StyleSheet.create({})
